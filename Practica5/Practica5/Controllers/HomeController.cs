f607cc0918281c44380881e30067f63e3072e03c
Administrador <Administrador@JORMAT>
2019-11-20 20:37:15 -0400

    Creacion de base de datos y controles

--- Practica5/Practica5/Controllers/HomeController.cs
+++ Practica5/Practica5/Controllers/HomeController.cs
@@ -13,18 +13,36 @@ namespace Practica5.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
+        //agracion de mis metodos
+        public ActionResult Agenda()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Evento()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+    
+        
+       
+
     }
 }
